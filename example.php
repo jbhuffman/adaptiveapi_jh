b0ae4e689ece629f9bafc7e5a72331c9e916410a
Jarad Huffman <jbhuffman@gmail.com>
2015-01-13 16:07:02 -0600

    updated the require call

--- example.php
+++ example.php
@@ -2,9 +2,12 @@
 /*
  * example script for using the Adaptive API
  */
+require_once 'adaptive.php';
+
 use \JHuffman\API;
 
 $user = 'adaptiveuser';
 $pass = 'adaptivepassword';
 
+// usage
 $levels = Adaptive::exportLevels($user, $pass);
